c5f316eabf559e4305a8f88a58d7eb3f00bd3acb
Ella <ellacalugay@gmail.com>
2023-04-23 20:32:27 +0800

    Pseudocode

--- Multiple Line Text.py
+++ Multiple Line Text.py
@@ -1,2 +1,17 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #4 | PROBLEM 3
 # Writing a method in python that writes multiple line of text contents into another text file.
+
+# Pseudocode
+# Open a file "mylife.txt" in write mode.
+# Create a while loop with a True condition.
+# Inside the loop, ask the user to input a line of text.
+    # Write the entered line into the file using the write() function.
+    # Ask the user if they want to enter another line of input.
+        # Check if the user wants to continue.
+            # exit the loop and continue executing the code
+        # Check if the user wants to stop.
+            # exit the loop and stop the execution of the code
+        # If the user enters an invalid input, prompt them to enter a valid one.
+    # Check if the user wants to stop again (in case they entered 'y' in the previous loop).
+            # exit the outer loop and stop the execution of the code
+# End of the code.
